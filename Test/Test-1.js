80d1739606a6625c43dc07202ae71637204b73c2
Kenindra Mahiswara <kenindramahiswara@gmail.com>
2026-04-22 18:54:45 +0700

    feat: added Hook

--- Test/Test-1.js
+++ Test/Test-1.js
@@ -5,9 +5,12 @@ const chrome = require('selenium-webdriver/chrome')
 describe('Saucedemo Automation Test', function(){
     let driver;
     let options = new chrome.Options();
+    let userName = 'standard_user'
+    let password = 'secret_sauce'
 
-    it('Open Saucedemo page and verify the title', async function () {
+    beforeEach(async() =>{
         options.addArguments('--incognito');
+        //options.addArguments('--headless');
         driver = await new Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
@@ -15,13 +18,19 @@ describe('Saucedemo Automation Test', function(){
 
         await driver.get('https://www.saucedemo.com/')
         await driver.manage().window().fullscreen()
+    })
 
+    afterEach(async() => {
+        await driver.quit()
+    })
+
+    it('Open Saucedemo page and verify the title', async function () {
         let inputUsername = await driver.findElement(By.xpath('//*[@id="user-name"]'))
         let inputPassword = await driver.findElement(By.xpath('//*[@id="password"]'))
         let loginButton = await driver.findElement(By.xpath('//*[@id="login-button"]'))
 
-        await inputUsername.sendKeys('standard_user')
-        await inputPassword.sendKeys('secret_sauce')
+        await inputUsername.sendKeys(userName)
+        await inputPassword.sendKeys(password)
         await loginButton.click()
 
         await driver.wait(until.elementLocated(By.className('app_logo')), 50000)
@@ -29,26 +38,15 @@ describe('Saucedemo Automation Test', function(){
 
         await appLogo.isDisplayed()
         await driver.takeScreenshot()
-    
-        await driver.quit()
     });
 
         it('Open Saucedemo page and sort the product', async function () {
-        options.addArguments('--incognito');
-        driver = await new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(options)
-            .build();
-
-        await driver.get('https://www.saucedemo.com/')
-        await driver.manage().window().fullscreen()
-
         let inputUsername = await driver.findElement(By.xpath('//*[@id="user-name"]'))
         let inputPassword = await driver.findElement(By.xpath('//*[@id="password"]'))
         let loginButton = await driver.findElement(By.xpath('//*[@id="login-button"]'))
 
-        await inputUsername.sendKeys('standard_user')
-        await inputPassword.sendKeys('secret_sauce')
+        await inputUsername.sendKeys(userName)
+        await inputPassword.sendKeys(password)
         await loginButton.click()
 
         await driver.wait(until.elementLocated(By.xpath('//*[@data-test="product-sort-container"]')), 50000)
@@ -56,9 +54,30 @@ describe('Saucedemo Automation Test', function(){
         await productSort.isDisplayed()
         
         await productSort.click()
-        await productSort.findElement(By.css("option[value='za']")).click()
-        await driver.takeScreenshot()
+        await productSort.findElement(By.css("option[value='hilo']")).click()
 
-        await driver.quit()
+        await driver.takeScreenshot()
+    });
+
+    it('Add item into Cart', async function () {
+        let inputUsername = await driver.findElement(By.xpath('//*[@id="user-name"]'))
+        let inputPassword = await driver.findElement(By.xpath('//*[@id="password"]'))
+        let loginButton = await driver.findElement(By.xpath('//*[@id="login-button"]'))
+
+        await inputUsername.sendKeys(userName)
+        await inputPassword.sendKeys(password)
+        await loginButton.click()
+
+        let addToCartbutton = await driver.findElement(By.xpath('//*[@id="add-to-cart-sauce-labs-backpack"]'))
+        await addToCartbutton.click()
+
+        let shoppingCart = await driver.findElement(By.xpath('//*[@data-test="shopping-cart-link"]'))
+        await shoppingCart.click()
+
+        await driver.wait(until.elementLocated(By.xpath('//*[@data-test="inventory-item-name"]')), 50000)
+        let itemName = await driver.findElement(By.xpath('//*[@data-test="inventory-item-name"]'))
+
+        await itemName.isDisplayed()
+        await driver.takeScreenshot()
     });
 });
